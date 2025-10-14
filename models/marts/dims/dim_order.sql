5122e715b0752bf954bafdde99198480fe25b3c4
Hatsune Andrew <truonghaonhien2822004@gmail.com>
2025-10-15 00:22:17 +0700

    chore + feat: reformat and remove dups
    removing dups is unnecessary since the staging models for territories
    and sales headers have assure the uniqueness of IDs

--- models/marts/dims/dim_order.sql
+++ models/marts/dims/dim_order.sql
@@ -1,6 +1,13 @@
-select
-    sales.SalesOrderID,
-    sales.TerritoryName,
-    sales.TerritoryGroup,
-    sales.OnlineOrderFlag
-from {{ ref('int_sales__join') }} as sales;
+{{ config(
+    cluster_by=["TerritoryName", "TerritoryGroup"]
+) }}
+WITH dups as (
+    SELECT
+        sales.SalesOrderID,
+        sales.TerritoryName,
+        sales.TerritoryGroup,
+        sales.OnlineOrderFlag
+    FROM {{ ref('int_sales__join') }} as sales
+)
+
+SELECT DISTINCT * FROM dups
