d96fe8d549c644599f88e02898da57b7b1e57989
Hatsune Andrew <truonghaonhien2822004@gmail.com>
2025-10-21 22:37:44 +0700

    fix: silly string in ProductSize of dim_product

--- models/staging/stg_production__product.sql
+++ models/staging/stg_production__product.sql
@@ -2,7 +2,7 @@ SELECT
     CAST(ProductID as INT) as ProductID,
     CAST(Name as STRING) as ProductName,
     CAST(Color as STRING) as Color,
-    CAST("Size" as STRING) as ProductSize,
+    CAST(Size as STRING) as ProductSize,
     CAST(ProductSubCategoryID as INT) ProductSubCategoryID,
     CAST(ProductModelID as INT) ProductModelID
 FROM Production.Product
