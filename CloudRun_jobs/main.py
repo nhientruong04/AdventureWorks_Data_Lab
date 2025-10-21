40e09bea087504494c891e33e83ff32eb4c9bb00
Hatsune Andrew <truonghaonhien2822004@gmail.com>
2025-10-21 18:43:51 +0700

    feat: new query and new derived AssociationRules table schema

--- CloudRun_jobs/main.py
+++ CloudRun_jobs/main.py
@@ -4,6 +4,34 @@ from mlxtend.frequent_patterns import fpgrowth, association_rules
 from loguru import logger
 import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
+import hashlib
+from datetime import date
+
+
+def build_query(fact_table: str, territory_id: int, date_filter: int) -> str:
+    conditions = list()
+    if territory_id is not None:
+        conditions.append(f"TerritoryID = {territory_id}")
+    if date_filter is not None:
+        conditions.append(f"OrderDateKey < {date_filter}")
+
+    condition_clause = "WHERE " + \
+        " AND ".join(conditions) if len(conditions) > 0 else ""
+
+    query = f"""
+    SELECT SalesOrderID, ARRAY_AGG(ProductID) AS products
+    FROM {fact_table}
+    {condition_clause}
+    GROUP BY SalesOrderID
+    """
+
+    return query
+
+
+def hash_rule(row):
+    ants = ', '.join(map(str, row["antecedents"]))
+    cons = ', '.join(map(str, row["consequents"]))
+    return hashlib.sha256((ants+cons).encode()).hexdigest()
 
 
 def main():
@@ -11,52 +39,50 @@ def main():
         description="Run Association Rules mining on BigQuery sales data."
     )
     parser.add_argument(
-        "project_id", help="Google Cloud project ID containing the dataset"
+        "project_id", help="Google Cloud project ID."
     )
     parser.add_argument(
         "dataset", help="BigQuery dataset name containing fact_sales and dim_territory tables"
     )
     parser.add_argument(
-        "--region",
-        default="total",
-        help="Region (territory) to process, or 'total' for all regions",
+        "--territory_id",
+        help="Territory id of the territory to process", type=int
+    )
+    parser.add_argument(
+        "--date_filter",
+        help="Max datekey for transactions, serve for demo only", type=int
     )
     args = parser.parse_args()
 
+    date_filter = None
+    if args.date_filter is not None:
+        try:
+            assert args.date_filter*1e-7 > 1
+        except AssertionError:
+            logger.error("Invalid date format. Use YYYYMMDD.")
+            return
+        date_filter = args.date_filter
+
     project_id = args.project_id
     dataset = args.dataset
-    region = args.region
+    territory_id = args.territory_id
+    fact_table = f"`{project_id}.{dataset}.fact_sales`"
 
-    logger.info(f"Running Association Rules mining job for project={
-                project_id}, dataset={dataset}, region={region}")
+    logger.info(f"Running Association Rules mining job for fact_table: \
+    {fact_table}, territory_id={territory_id}, date_filter={date_filter}")
 
     bq = bigquery.Client(project=project_id)
-
-    query = str()
-    if region.lower() == "total":
-        query = f"""
-        SELECT SalesOrderID, ARRAY_AGG(ProductID) AS products
-        FROM `{project_id}.{dataset}.fact_sales`
-        GROUP BY SalesOrderID
-        """
-    else:
-        query = f"""
-        SELECT SalesOrderID, ARRAY_AGG(ProductID) AS products
-        FROM `{project_id}.{dataset}.fact_sales` as fs
-        JOIN `{project_id}.{dataset}.dim_territory` as dt
-            ON fs.TerritoryID = dt.TerritoryID
-        WHERE TerritoryName = '{region}'
-        GROUP BY SalesOrderID
-        """
+    query = build_query(fact_table, territory_id, date_filter)
 
     df = bq.query(query).to_dataframe()
 
     if df.empty:
-        logger.warn("No data returned for this region. Exiting.")
+        logger.warning("No data returned for this territory_id. Exiting.")
         return
     else:
         logger.info(f"Retrieved {len(df)} transactions")
 
+    # should discuss more about thresholds
     min_sup = 0.015 if len(df) >= 1000 else 0.15
 
     te = TransactionEncoder()
@@ -70,7 +96,7 @@ def main():
     )
 
     if rules.empty:
-        logger.err("No association rules found for this region. \
+        logger.error("No association rules found for this territory_id. \
             Try lower the min_sup or min_conf.")
         return
 
@@ -80,19 +106,22 @@ def main():
         result = result.sort_values(by=["support", "lift"], ascending=[
                                     False, False]).head(400)
 
+    result["territory_id"] = territory_id
+    result["territory_id"] = result["territory_id"].astype("Int64")
+    result["update_at"] = int(date.today().strftime("%Y%m%d"))
     result["antecedents"] = result["antecedents"].apply(
         lambda x: sorted(list(x)))
     result["consequents"] = result["consequents"].apply(
         lambda x: sorted(list(x)))
+    result["rule_hash"] = result.apply(hash_rule, axis=1)
 
-    suffix = "Total" if region.lower() == "total" else region
-    table_id = f"{project_id}.analytics.AssociationRules_{suffix}"
+    table_id = f"{project_id}.{dataset}_analytics.AssociationRules"
     logger.info(f"Writing results to {table_id}")
 
     job = bq.load_table_from_dataframe(
         result,
         table_id,
-        job_config=bigquery.LoadJobConfig(write_disposition="WRITE_TRUNCATE"),
+        job_config=bigquery.LoadJobConfig(write_disposition="WRITE_APPEND"),
     )
     job.result()
     logger.info("Upload completed")
