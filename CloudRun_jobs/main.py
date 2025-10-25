6dc473bcd36f138b5d048d3af4d71a90eda5da49
Hatsune Andrew <truonghaonhien2822004@gmail.com>
2025-10-25 23:52:42 +0700

    feat: add 8 weeks window and retries limit

--- CloudRun_jobs/main.py
+++ CloudRun_jobs/main.py
@@ -1,5 +1,6 @@
 import argparse
 import math
+import uuid
 from google.cloud import bigquery
 from mlxtend.frequent_patterns import fpgrowth, association_rules
 import numpy as np
@@ -7,15 +8,34 @@ from loguru import logger
 import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
 import hashlib
-from datetime import date
+import time
+from datetime import date, datetime, timedelta
+from google.api_core.exceptions import TooManyRequests, ServiceUnavailable, InternalServerError
 
 
-def build_query(fact_table: str, territory_id: int, date_filter: int) -> str:
+MAX_RETRIES = 3
+INITIAL_DELAY = 15
+
+
+def build_query(fact_table: str, territory_id: int, date_filter: int,
+                window: int = None) -> str:
+    # window is the length of the window in days
     conditions = list()
     if territory_id is not None:
         conditions.append(f"TerritoryID = {territory_id}")
+
     if date_filter is not None:
         conditions.append(f"OrderDateKey < {date_filter}")
+
+        if window is not None:
+            max_date = datetime.strptime(str(date_filter), "%Y%m%d")
+            window_span = timedelta(window)
+            min_date = (max_date - window_span).strftime("%Y%m%d")
+            logger.info(
+                f"Recieved window {window} days, min_date is {min_date}"
+            )
+
+            conditions.append(f"OrderDateKey > {int(min_date)}")
 
     condition_clause = "WHERE " + \
         " AND ".join(conditions) if len(conditions) > 0 else ""
@@ -57,8 +77,9 @@ def mine_rules(basket, single_itemset_flag=True,
                min_sup=0.02, max_rules=35):
     logger.info(f"Running with parameters: min_sup={min_sup},\
     max_rules={max_rules}, single-item set={single_itemset_flag}.")
+    max_retries = 5
 
-    while min_sup >= 0.01:
+    while min_sup >= 0.01 and max_retries > 0:
         frequent_itemsets = fpgrowth(
             basket, min_support=min_sup, use_colnames=True)
         rules = association_rules(
@@ -85,11 +106,45 @@ def mine_rules(basket, single_itemset_flag=True,
         else:
             logger.info(
                 f"Too few rules, need at least {int(0.8*max_rules)} \
-                rules, got {len(result)} instead. Lowering min_sup to {min_sup/2}"
+                rules, got {len(result)} instead. Lowering min_sup to {min_sup*0.75}"
             )
-            min_sup /= 2
+            min_sup *= 0.75
+            max_retries -= 1
 
     return result
+
+
+def write_with_retry(bq_client, result_df, table_id):
+    logger.info(f"Writing results to {table_id}, {len(result_df)} rules.")
+
+    for attempt in range(1, MAX_RETRIES + 1):
+        try:
+            job = bq_client.load_table_from_dataframe(
+                result_df, table_id,
+                job_config=bigquery.LoadJobConfig(
+                    write_disposition="WRITE_APPEND")
+            )
+            job.result()
+            logger.info("Upload completed.")
+            return job
+
+        except (TooManyRequests, ServiceUnavailable, InternalServerError) as e:
+            if attempt < MAX_RETRIES:
+                wait_time = INITIAL_DELAY * \
+                    (2 ** (attempt - 1))  # exponential backoff
+                logger.warning(
+                    f"Attempt {attempt}/{MAX_RETRIES} failed with error: {e}. "
+                    f"Retrying in {wait_time} seconds..."
+                )
+                time.sleep(wait_time)
+            else:
+                logger.error(
+                    "Max retries reached. Upload failed.")
+                raise
+
+        except Exception as e:
+            logger.error(f"Upload failed with non-retryable error: {e}")
+            raise
 
 
 def main():
@@ -110,6 +165,11 @@ def main():
         "--date_filter",
         help="Max datekey for transactions, serve for demo only", type=int
     )
+    parser.add_argument(
+        "--window",
+        help="Window determines the time window for transactions in days,\
+        with date_filter - window.", type=int
+    )
     args = parser.parse_args()
 
     date_filter = None
@@ -124,18 +184,23 @@ def main():
     project_id = args.project_id
     dataset = args.dataset
     territory_id = args.territory_id
+    window = args.window
     fact_table = f"`{project_id}.{dataset}.fact_sales`"
 
     logger.info(f"Running Association Rules mining job for fact_table: \
-    {fact_table}, territory_id={territory_id}, date_filter={date_filter}")
+    {fact_table}, territory_id={territory_id}, date_filter={date_filter}, \
+    window={window}")
 
     bq = bigquery.Client(project=project_id)
-    query = build_query(fact_table, territory_id, date_filter)
+    query = build_query(fact_table, territory_id, date_filter, window)
 
     df = bq.query(query).to_dataframe()
 
     if df.empty:
-        logger.warning("No data returned for this territory_id. Exiting.")
+        logger.warning(
+            "No data found for the query:\n"
+            f"{query}\n"
+            "Exiting.")
         return
     else:
         logger.info(f"Retrieved {len(df)} transactions.")
@@ -145,32 +210,33 @@ def main():
     te_ary = te.fit(df["products"]).transform(df["products"])
     basket = pd.DataFrame(te_ary, columns=te.columns_)
     min_sup = initialize_min_sup(df["products"].to_list())
-    result = mine_rules(basket, min_sup=min_sup)
+    max_rules = 20 if window is not None else 35
+    result = mine_rules(basket, min_sup=min_sup, max_rules=max_rules)
 
+    if len(result) == 0:
+        logger.info(f"No rules found for this territory {territory_id},"
+                    f"date_filter {date_filter} and window {window}.")
+        return
+
+    result["row_id"] = [str(uuid.uuid4()) for _ in range(len(result))]
     result["territory_id"] = territory_id
     result["territory_id"] = result["territory_id"].astype("Int64")
-    result["update_at"] = int(date.today().strftime("%Y%m%d"))
+    result["window"] = window
+    result["window"] = result["window"].astype("Int64")
+
+    if date_filter is None:
+        result["update_at"] = int(date.today().strftime("%Y%m%d"))
+    else:
+        result["update_at"] = date_filter
+
     result["antecedents"] = result["antecedents"].apply(
         lambda x: sorted(list(x)))
     result["consequents"] = result["consequents"].apply(
         lambda x: sorted(list(x)))
     result["rule_hash"] = result.apply(hash_rule, axis=1)
 
-    table_id = f"{project_id}.{dataset}_analytics.AssociationRules"
-    logger.info(f"Writing results to {table_id}, {len(result)} rules.")
-
-    job = bq.load_table_from_dataframe(
-        result,
-        table_id,
-        job_config=bigquery.LoadJobConfig(write_disposition="WRITE_APPEND"),
-    )
-
-    try:
-        job.result()
-        logger.info("Upload completed")
-    except Exception as e:
-        logger.error(f"Upload failed with error: {e}")
-        raise e
+    table_id = f"{project_id}.{dataset}_analytics.AssociationRulesScheduled"
+    write_with_retry(bq, result, table_id)
 
 
 if __name__ == "__main__":
