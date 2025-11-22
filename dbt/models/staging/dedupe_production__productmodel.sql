44b376382a94c2a3e3049945b1484ad43fb41552
Hatsune Andrew <truonghaonhien2822004@gmail.com>
2025-11-22 19:31:21 +0700

    feat: dedupe models for Production schema snapshots

---
+++ dbt/models/staging/dedupe_production__productmodel.sql
@@ -0,0 +1,41 @@
+{{
+    config(
+        materialized="ephemeral",
+        tags=["dedupe"]
+    )
+}}
+
+WITH
+hashed AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key([
+                'ProductModelID',
+                'Name'
+            ]) }} AS grain_id,
+        *
+    FROM {{ source("Production", "ProductModel") }}
+),
+
+mark_real_diffs AS (
+    SELECT
+        *,
+        CASE
+            WHEN grain_id !=
+                COALESCE(
+                    LAG(grain_id) OVER (PARTITION BY ProductModelID ORDER BY ModifiedDate),
+                    'first_row'
+                ) THEN 1
+            ELSE 0
+        END AS is_real_diff
+    FROM hashed
+),
+
+final AS (
+    SELECT
+        * EXCEPT(grain_id, is_real_diff),
+        grain_id AS _dedupe_grain_id
+    FROM mark_real_diffs
+    WHERE is_real_diff = 1
+)
+
+SELECT * FROM final
