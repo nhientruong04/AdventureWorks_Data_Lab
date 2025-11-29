2c4dbd4b17f9a3a214acbe83f106212458446a02
Hatsune Andrew <truonghaonhien2822004@gmail.com>
2025-11-29 21:52:52 +0700

    feat: add row key to dim_territory and unknown terr

--- dbt/models/marts/dims/dim_territory.sql
+++ dbt/models/marts/dims/dim_territory.sql
@@ -1,2 +1,23 @@
+WITH
+unknown AS (
+    SELECT
+        0 AS TerritoryKey,
+        0 AS TerritoryID,
+        'unknown' AS TerritoryName,
+        'unknown' AS TerritoryGroup,
+        'unknown' AS CountryRegionCode
+),
+
+final AS (
+    SELECT * FROM unknown
+
+    UNION ALL
+
+    SELECT
+    ROW_NUMBER() OVER (ORDER BY TerritoryID) AS TerritoryKey,
+    *
+    FROM {{ ref('stg_sales__salesterritory') }}
+)
+
 SELECT *
-FROM {{ ref('stg_sales__salesterritory') }}
+FROM final
