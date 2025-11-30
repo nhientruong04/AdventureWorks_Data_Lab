aea088698ffa1b0eb1f52326b257ba6644266130
Hatsune Andrew <truonghaonhien2822004@gmail.com>
2025-11-30 15:25:49 +0700

    feat: change snapshots join logic
    due to duplications. Could be either the faulty logic of the approach or
    my stupidity, though I think it's the latter. Will revise the logic of
    https://docs.getdbt.com/blog/joining-snapshot-complexity later when I
    can prove the current approach is fine.

--- dbt/models/staging/stg_snapshot__product.sql
+++ dbt/models/staging/stg_snapshot__product.sql
@@ -1,16 +1,7 @@
 WITH
-spine AS (
-    SELECT
-        p.*,
-        psc.ProductCategoryID
-    FROM {{ ref('ProductSnapshot')}} AS p
-    LEFT JOIN {{ ref('ProductSubcategorySnapshot')}} AS psc
-        ON p.ProductSubcategoryID = psc.ProductSubcategoryID
-),
-
 join_subcategory AS (
     {{ join_snapshots(
-        left_snapshot = 'spine',
+        left_snapshot = ref('ProductSnapshot'),
         right_snapshot = ref('ProductSubcategorySnapshot'),
         left_key = 'ProductSubcategoryID',
         right_key = 'ProductSubcategoryID',
@@ -18,7 +9,7 @@ join_subcategory AS (
         left_valid_to = 'valid_to',
         right_valid_from = 'valid_from',
         right_valid_to = 'valid_to',
-        right_attributes = ['Name']
+        right_attributes = ['Name', 'ProductCategoryID']
     ) }}
 ),
 
@@ -26,7 +17,7 @@ join_category AS (
     {{ join_snapshots(
         left_snapshot = 'join_subcategory',
         right_snapshot = ref('ProductCategorySnapshot'),
-        left_key = 'ProductCategoryID',
+        left_key = 'ProductSubcategorySnapshot__ProductCategoryID',
         right_key = 'ProductCategoryID',
         left_valid_from = 'valid_from',
         left_valid_to = 'valid_to',
