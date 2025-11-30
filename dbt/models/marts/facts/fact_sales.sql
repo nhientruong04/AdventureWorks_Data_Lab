5bd715a917a3c26b8de62daf1a9017724804cacc
Hatsune Andrew <truonghaonhien2822004@gmail.com>
2025-11-30 17:06:44 +0700

    feat: fact_sales with CTEs for obtaining keys from dimensions
    the incremental loading is not functioning correctly, due to the lack of
    product history. Since SCD 2 for dim_product is implemented after 2014,
    all orders placed before that time will be lost after the dim_product
    join logic. Can only be fixed by manually changing the ModifiedDate and
    SCD time of the Product-related snapshots.

--- dbt/models/marts/facts/fact_sales.sql
+++ dbt/models/marts/facts/fact_sales.sql
@@ -1,14 +1,45 @@
 {{
     config(
-        unique_key=["ProductID", "SalesOrderID"],
-        cluster_by=["ProductID", "SalesOrderID"]
+        unique_key=["ProductKey", "SalesOrderID"],
+        cluster_by=["ProductKey", "SalesOrderID"]
     )
 }}
-SELECT
-    * EXCEPT (OrderDate, ModifiedDate),
-    {{ convert_datekey('ModifiedDate') }} as ModifiedDateKey,
-    {{ convert_datekey('OrderDate') }} as OrderDateKey
-FROM {{ ref('int_sales__join') }}
-{% if is_incremental() %}
-    WHERE {{ convert_datekey('ModifiedDate') }} >= (SELECT MAX(ModifiedDateKey) FROM {{ this }})
-{% endif %}
+
+WITH
+new_records AS (
+    SELECT *
+    FROM {{ ref('int_sales__join') }}
+    {% if is_incremental() %}
+        WHERE {{ convert_datekey('OrderDate') }} >= (SELECT MAX(OrderDateKey)-7 FROM {{ this }})
+    {% endif %}
+),
+
+product_join AS (
+    SELECT
+        new_records.* EXCEPT(ProductID),
+        prod.ProductKey
+    FROM new_records
+    JOIN {{ ref('dim_product') }} AS prod
+        ON new_records.ProductID = prod.ProductID
+        AND new_records.OrderDate >= prod.ValidFrom
+        AND new_records.OrderDate < prod.ValidTo
+),
+
+terr_join AS (
+    SELECT
+        p.* EXCEPT(TerritoryID),
+        t.TerritoryKey
+    FROM product_join AS p
+    JOIN {{ ref('dim_territory') }} AS t
+        ON p.TerritoryID = t.TerritoryID
+),
+
+final AS (
+    SELECT
+        * EXCEPT(OrderDate),
+        {{ convert_datekey('OrderDate') }} AS OrderDateKey,
+        CURRENT_DATETIME() AS LoadDatetime,
+    FROM terr_join
+)
+
+SELECT * FROM final
