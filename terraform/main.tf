f3ff236e1f961f5b85e9d13f3775ea30411fc8b9
Hatsune Andrew <truonghaonhien2822004@gmail.com>
2025-12-12 22:53:37 +0700

    fix: allow airflow iap traffic
    can be removed later

--- terraform/main.tf
+++ terraform/main.tf
@@ -121,7 +121,7 @@ resource "google_compute_firewall" "ssh_rule" {
   network     = google_compute_network.vpc_network.id
   allow {
     protocol = "tcp"
-    ports    = ["22", "8000"]
+    ports    = ["22", "8000", "8080"]
   }
   source_ranges      = ["35.235.240.0/20"]
   destination_ranges = ["0.0.0.0/0"]
