d7cf023ac0d4a318fbb31c16dd0cffc80744079b
Hatsune Andrew <truonghaonhien2822004@gmail.com>
2025-12-13 19:02:50 +0700

    feat: dbt-trigger task using BashOperator
    just a test version, use BashOperator to avoid creating service key

--- Airflow/dags/dbt-trigger.py
+++ Airflow/dags/dbt-trigger.py
@@ -1,6 +1,6 @@
 from airflow import DAG
-from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime
+from airflow.providers.standard.operators.bash import BashOperator
 from airflow.sdk import Variable
 
 with DAG(
@@ -8,13 +8,15 @@ with DAG(
     start_date=datetime(2025, 10, 22),
     schedule=None,
 ) as dag:
-    t1 = DockerOperator(
+    dbt_run = BashOperator(
         task_id="dbt-trigger",
-        image="dbt-image:latest",
-        auto_remove="force",
-        environment={
+        bash_command="""
+        source /opt/venvs/dbt-env/bin/activate
+        cd /opt/dbt
+        dbt run --profiles-dir /opt/dbt --select stg_sales__salesorderheader
+        """,
+        env={
             "BQ_PROJECT_ID": Variable.get("bq_project_id"),
             "BQ_PROJECT_REGION": Variable.get("bq_project_region"),
         },
-        mount_tmp_dir=False
     )
